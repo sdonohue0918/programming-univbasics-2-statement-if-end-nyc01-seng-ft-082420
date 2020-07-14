36fea4c929cc2ed22fb5c3c7a3f8a11de5b8aa94
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-07-14 14:40:39 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -9,11 +9,14 @@
 #puts "Code after if...end"
 
 
-if true
+
+
+current_time = Time.now
+current_time = current_time.to_i
+
+if current_time % 2 == 0
   puts "Even!"
 else
   puts "Odd!"
 end
 
-current_time = Time.now
-current_time = current_time.to_i
