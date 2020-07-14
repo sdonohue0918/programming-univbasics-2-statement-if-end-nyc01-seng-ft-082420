8faba5c2fddb6223649921fa1c2a43388accd909
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-07-14 14:42:16 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,21 @@
-# Write your solution here
+# Write your solution hererun_code_inside = true
+
+#run_code_inside = true
+
+#puts "Code before if...end"
+#if run_code_inside
+  #puts "code inside"
+#end
+#puts "Code after if...end"
+
+
+
+
+current_time = Time.now
+current_time = current_time.to_i
+
+if current_time % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
